1d4bfc88a5e1f5c4e662902563f7e1e6e172a95e
Christian Thrige Nielsen <149200558+CN-Thrige@users.noreply.github.com>
2026-04-10 08:37:25 +0200

    Add files via upload

---
+++ Bash scripts/updateme.sh
@@ -0,0 +1,15 @@
+#! /bin/bash 
+
+echo Would you like to update Ubuntu?
+
+sudo apt  update -y
+
+echo Would you like to upgrade Ubuntu?
+
+sudo apt upgrade -y
+
+echo would you like to remove junk?
+
+sudo apt autoremove 
+
+echo end (:
