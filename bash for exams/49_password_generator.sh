72433131b5b385b164a630dc7d29a8160aec1706
Christian Thrige Nielsen <149200558+CN-Thrige@users.noreply.github.com>
2026-07-18 17:07:17 +0200

    Update 49_password_generator.sh

--- bash for exams/49_password_generator.sh
+++ bash for exams/49_password_generator.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # Password Generator
-# Explanation: Generates a random password of specified length using characters from a set.
+# Explanation: Asks for confirmation, then generates a random password
+# of a specified length using characters from a set.
 
-length=12
-characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
-password=$(head /dev/urandom | tr -dc "$characters" | head -c "$length")
-echo "Generated Password: $password"
+echo "Would you like to make a new password?"
+read -p "Are you sure? " -n 1 -r
+echo    # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    length=12
+    characters="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+    password=$(head /dev/urandom | tr -dc "$characters" | head -c "$length")
+    echo "Generated Password: $password"
+else
+    echo "Password generation cancelled."
+fi
